c84a2d1eeb4b0068de27f7f7d9edbb4d6bc97e95
khainguyenxuan86-cyber <khai.nguyenxuan86@gmail.com>
2026-09-17 15:02:45 +0700

    Modify unscramble questions and add new entry
    Updated images and words in unscramble questions, added new unscramble question.

--- questions.js
+++ questions.js
@@ -7,16 +7,16 @@ const questions = [
     id: "unscramble1",
     type: "unscramble",
     question: "Put the words in the correct order.",
-    image: "images/chair.png",
-    words: ["living", "There", "a", "is", "chair", "in", "the", "room"],
-    answer: ["There", "is", "a", "chair", "in", "the", "living", "room"]
+    image: "images/1.png",
+    words: ["living", "There", "a", "is", "chair", "in", "the", "room."],
+    answer: ["There", "is", "a", "chair", "in", "the", "living", "room."]
   },
 
   {
     id: "unscramble2",
     type: "unscramble",
     question: "Put the words in the correct order.",
-    image: "images/books.png",
+    image: "images/2.png",
     words: ["table.", "books", "are", "There", "five", "on", "the"],
     answer: ["There", "are", "five", "books", "on", "the", "table."]
   },
@@ -25,7 +25,7 @@ const questions = [
     id: "unscramble3",
     type: "unscramble",
     question: "Put the words in the correct order.",
-    image: "images/laptops.png",
+    image: "images/3.png",
     words: ["bed.", "laptops", "are", "There", "two", "on", "the"],
     answer: ["There", "are", "two", "laptops", "on", "the", "bed."]
   },
@@ -34,74 +34,35 @@ const questions = [
     id: "unscramble4",
     type: "unscramble",
     question: "Put the words in the correct order.",
-    image: "images/lamp.png",
-    words: ["isn't", "There", "a", "shelf", "lamp", "on", "the"],
-    answer: ["There", "isn't", "a", "lamp", "on", "the", "shelf"]
+    image: "images/4.png",
+    words: ["isn't", "There", "a", "shelf.", "lamp", "on", "the"],
+    answer: ["There", "isn't", "a", "lamp", "on", "the", "shelf."]
   },
 
     {
     id: "unscramble5",
     type: "unscramble",
     question: "Put the words in the correct order.",
-    image: "images/bathroom.png",
-    words: ["aren't", "There", "bathroom", "any", "tables", "in", "the"],
-    answer: ["There", "aren't", "any", "tables", "in", "the", "bathroom"]
-  },
-  // =========================
-  // WRITING
-  // =========================
-  // The checker does NOT require one exact sentence.
-  // It checks the structure defined in "requirements".
-   {
-    id: "writing1",
-    type: "writing",
-    question: "Look at the picture and write one sentence.",
-    image: "images/desk.png",
-    template: "There is a/an + noun + in/on .......",
-    requirements: {
-      subjects: ["There"],
-      verbs: ["is"],
-      article: ["a"],
-      preposition: ["in"],
-      keywords: ["desk", "bedroom"],
-      punctuation: ".",
-      minWords: 6
-    },
-    sampleAnswer: "There is a desk in the bedroom."
+    image: "images/5.png",
+    words: ["aren't", "There", "bathroom.", "any", "tables", "in", "the"],
+    answer: ["There", "aren't", "any", "tables", "in", "the", "bathroom."]
   },
 
-  {
-    id: "writing2",
-    type: "writing",
-    question: "Look at the picture and write one sentence.",
-    image: "images/pens.png",
-    template: "There are + 'number' + plural noun + in/on .......",
-    requirements: {
-      subjects: ["There"],
-      verbs: ["are"],
-      preposition: ["on"],
-      number: ["two", "2"],
-      keywords: ["pens"],
-      punctuation: ".",
-      minWords: 6
-    },
-    sampleAnswer: "There are two pens on the desk."
+    {
+    id: "unscramble6",
+    type: "unscramble",
+    question: "Put the words in the correct order.",
+    image: "images/6.png",
+    words: ["are", "There", "four", "pictures", "on", "the", "wall."],
+    answer: ["There", "are", "four", "pictures", "on", "the", "wall."]
   },
 
-  {
-    id: "writing3",
-    type: "writing",
-    question: "Look at the picture and write one sentence.",
-    image: "images/wall.png",
-    template: "There aren't any + plural noun + in/on .......",
-    requirements: {
-      subjects: ["There"],
-      verbs: ["aren't", "are not"],
-      preposition: ["on"],
-      keywords: ["pictures", "any", "wall"],
-      punctuation: ".",
-      minWords: 6
-    },
-    sampleAnswer: "There aren't any pictures on the wall."
+      {
+    id: "unscramble7",
+    type: "unscramble",
+    question: "Put the words in the correct order.",
+    image: "images/7.png",
+    words: ["There", "is", "a", "blue", "blanket", "on", "the", "bed."],
+    answer: ["There", "is", "a", "blue", "blanket", "on", "the", "bed."]
   },
 ];
